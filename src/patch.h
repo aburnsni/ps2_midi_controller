ef2a0bdd5391fcdd3285fec1f8fbfeee5fbf9ed2
Andrew Burns <aburnsni@gmail.com>
2020-03-28 16:26:08 +0000

    Patch change for SSD

--- src/patch.h
+++ src/patch.h
@@ -4,11 +4,11 @@ int dpad_down = DRUM_BASS_1;
 int dpad_left = SNARE_RIM;
 
 int button_triangle = CYM_CRASH_2;
-int button_circle = CYM_CRASH_1;
+int button_circle = CYM_RIDE_1;
 int button_cross = HIHAT_CLOSED;
 int button_square = HIHAT_OPEN;
 
-int rtrig1 = TOM_HI;
-int rtrig2 = TOM_HIMID;
-int ltrig1 = TOM_LOWMID;
-int ltrig2 = TOM_LOW;
+int rtrig1 = TOM_HIMID;
+int rtrig2 = TOM_LOWMID;
+int ltrig1 = TOM_FLOOR_HIGH;
+int ltrig2 = TOM_FLOOR_LOW;
